8a47e9f0a8dab5cf151528f5974528d733d4eb48
mamunur-roshid <nijummamun7@gmail.com>
2023-07-15 00:01:59 +0600

    again start agency project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(".main__slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<i class="fa-solid fa-angle-left left"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right right"></i>',
+});
