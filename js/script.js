4b825f1f726505b5947b265ad06e76a09de242f2
mamunur-roshid <nijummamun7@gmail.com>
2023-07-26 14:42:43 +0600

    added responsive

--- js/script.js
+++ js/script.js
@@ -4,4 +4,24 @@ $(".main__slider").slick({
   arrows: true,
   prevArrow: '<i class="fa-solid fa-angle-left left"></i>',
   nextArrow: '<i class="fa-solid fa-angle-right right"></i>',
+  responsive: [
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$(".banner__slider").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3000,
+  arrows: false,
 });
