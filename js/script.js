1c81cec0c9cd3a9acef05065f2d87d271ef33767
mamunur-roshid <nijummamun7@gmail.com>
2023-07-30 20:02:38 +0600

    added some responsive css

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-$(".main__slider").slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  arrows: true,
-  prevArrow: '<i class="fa-solid fa-angle-left left"></i>',
-  nextArrow: '<i class="fa-solid fa-angle-right right"></i>',
-  responsive: [
-    {
-      breakpoint: 575,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-});
-
 $(".banner__slider").slick({
   dots: true,
   infinite: true,
@@ -24,4 +7,23 @@ $(".banner__slider").slick({
   autoplay: false,
   autoplaySpeed: 3000,
   arrows: false,
+});
+
+$(".main__slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: "0px",
+  arrows: true,
+  prevArrow: '<i class="fa-solid fa-angle-left left"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right right"></i>',
+  responsive: [
+    {
+      breakpoint: 575.98,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
